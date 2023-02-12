bca5775fe06af5afeb6c1dfe305251396b714b55
Gabriela Ribeiro <103806445+D3vGab@users.noreply.github.com>
2023-02-12 12:08:11 -0400

    Bot cars script
    The cars return to the starting position when they reach the end of the track.

--- Assets/Challenge 1/Scripts/Cars.cs
+++ Assets/Challenge 1/Scripts/Cars.cs
@@ -6,15 +6,24 @@ public class Cars : MonoBehaviour
 {
     public float speed;
     public string inputID;
+    private Vector3 position; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(inputID == "car1") {
+            position = new Vector3(-3, 0, 183);
+        }else {
+            position = new Vector3(5, 1, 183);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if(transform.position.z <= -16){
+            transform.position = position;
+        }
     }
 }
